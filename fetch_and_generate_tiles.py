e92f61b304df377915cff68994441ca3e95063e1
Stuart Lynn <stuart.lynn@gmail.com>
2018-07-20 08:27:32 -0400

    fixing lots of bugs in the generate tile script

--- fetch_and_generate_tiles.py
+++ fetch_and_generate_tiles.py
@@ -2,18 +2,18 @@ from urllib.request import urlretrieve
 import subprocess
 import os
 
-def download_tile_sets():
+def download_tile_sets(tile_sets_to_load):
 
-    print('tile sets to load ', target_tile_sets)
+    print('tile sets to load ', tile_sets_to_load)
 
     base_url = 'https://s3.amazonaws.com/metro-extracts.nextzen.org/2018-05-19-13-00'
     osm_path = '/data/osm'
     downloaded_targets = []
     for tile_set in tile_sets_to_load:
-        target_path = "{osm_path}/{tile_set}".format(osm_path=osm_path, tile_set=tile_set.strip())
+        target_path = "{osm_path}/{tile_set}".format(osm_path=osm_path, tile_set=tile_set)
         if os.path.isfile(target_path)==False:
-            print('downloading osm files for ', tile_set.strip())
-            url = "{base_url}/{tile_set}".format(base_url=base_url, tile_set=tile_set.strip())
+            print('downloading osm files for ', tile_set)
+            url = "{base_url}/{tile_set}".format(base_url=base_url, tile_set=tile_set)
             print('downloading osm files for ', tile_set , ' from ', url)
             urlretrieve(url,
                         target_path)
@@ -24,25 +24,26 @@ def download_tile_sets():
 
 def get_list_of_tiles_to_load():
     with open('/conf/tile_sets.csv', 'r') as tile_file:
-        target_tile_sets = tile_file.readlines()
+        target_tile_sets = [ tile.strip() for tile in tile_file.readlines() if len(tile.strip())>0]
 
     with open('/conf/loaded_tile_sets.csv', 'r') as loaded_file:
-        loaded_tile_sets = loaded_file.readlines()
+        loaded_tile_sets = [ tile.strip() for tile in loaded_file.readlines() if len(tile.strip())>0]
 
     tile_sets_to_load = list(set(target_tile_sets) - set(loaded_tile_sets))
 
-    return [ tile +'.osm.pdf' for tile in tile_sets_to_load ]
+    return [ tile +'.osm.pbf' for tile in tile_sets_to_load ]
 
 if __name__ == "__main__":
+
+    if not os.path.isdir('/data/osm'):
+        os.mkdir('/data/osm')
+
     TILE_SET_LIST = get_list_of_tiles_to_load()
     download_tile_sets(TILE_SET_LIST)
     subprocess.call(['valhalla_build_tiles',
                      '-c',
                      '/conf/valhalla.json'
-                    ]+ TILE_SET_LIST)
-    subprocess.call(['tar',
-                     '-cvf',
-                     '/data/valhalla/tiles.tar',
-                     '/data/vahalla/*'
-                    ]+ TILE_SET_LIST)
+                    ]+ ['/data/osm/{}'.format(tile) for tile in TILE_SET_LIST])
+    subprocess.call([ 'tar', '-cvf', '/data/valhalla/tiles.tar' , '/data/valhalla/'] )
+
 
